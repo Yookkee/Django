ecf31cce2a589b831cdc103cf6600a8e89eeb71f
Yookkee <japroc@mail.ru>
2016-07-06 09:40:44 +0300

    spaces o_O
    -now api allows to write text and title with spaces

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,6 @@ app_name = "api"
 urlpatterns = [
     url(r"^auth/(?P<secret_key>\w+)$", views.auth, name='auth'),
     url(r"^(?P<secret_key>[\w-]+)/list/$", views.list, name='list'),
-    url(r"^(?P<secret_key>[\w-]+)/delete/(?P<post_id>[\w-]+)/$", views.delete, name='delete'),
-    url(r"^(?P<secret_key>[\w-]+)/add/(?P<title>[\w-]+)/(?P<text>[\w-]+)/$", views.add, name='add'),
+    url(r"^(?P<secret_key>[\w-]+)/delete/(?P<post_id>[\w|\W]+)/$", views.delete, name='delete'),
+    url(r"^(?P<secret_key>[\w-]+)/add/(?P<title>[\w|\W]+)/(?P<text>[\w|\W]+)/$", views.add, name='add'),
 ]
