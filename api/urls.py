bf5732471e830ad213a452e1306d66c935d6785d
Yookkee <japroc@mail.ru>
2016-07-08 01:33:00 +0300

    API auth bugfix
    Old:
    "api/auth/<secret_key>" - correct queary
    "api/auth/<secret_key>/" - incorrect queary
    Now:
    -both queries are correct

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from api import views
 app_name = "api"
 
 urlpatterns = [
-    url(r"^auth/(?P<secret_key>\w+)$", views.auth, name='auth'),
+    url(r"^auth/(?P<secret_key>\w+)/$", views.auth, name='auth'),
     url(r"^(?P<secret_key>[\w-]+)/list/$", views.list, name='list'),
     url(r"^(?P<secret_key>[\w-]+)/delete/(?P<post_id>[\w|\W]+)/$", views.delete, name='delete'),
     url(r"^(?P<secret_key>[\w-]+)/add/(?P<title>[\w|\W]+)/(?P<text>[\w|\W]+)/$", views.add, name='add'),
